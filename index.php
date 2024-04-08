c272d7fa4b8a8840068b985681c09bdd0de6c327
erwann quin <erwann.quin@supinfo.com>
2024-04-08 18:39:55 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="styleIndex.css">
+        <title>Page principale</title>
+    </head>
+    <body class="grid-general">
+        <header>
+            <a href="index.php" class="logo"><img src="logo.png" alt="Retourner sur la page principale"></a>
+            <h1>Titre du site</h1>
+            <a class="search" href="search.php">Rechercher</a>
+            <a class="connection">Se connecter</a>
+            <a class="cart" href="cart.php" class="logo"><img src="cart.png" alt="Accéder au panier"></a>
+        </header>
+        <main class="grid-page-principale">
+            <h1>H1</h1><br>
+            <h2>H2</h2>
+        </main>
+    </body>
+</html>
